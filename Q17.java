828465bf004f445aee62daa4d5a795792101d0c3
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-08-15 19:16:13 +0530

    Added solution to `Q17`

---
+++ Q17.java
@@ -0,0 +1,27 @@
+/*
+Question 17
+
+Print the following pattern :
+
+34567
+3456
+345
+34
+3
+
+*/
+
+public class Q17
+{
+	public static void main(String args[])
+	{
+		for(int i = 7;i >= 3;i--)
+		{
+			for(int j = 3;j <= i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
