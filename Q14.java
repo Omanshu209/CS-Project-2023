da4fdf54ee8f24a78021d8b6d7a28bb63ee33037
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-08-16 20:26:07 +0530

    Added solution to `Q14`

---
+++ Q14.java
@@ -0,0 +1,36 @@
+/*
+Question 14
+
+W.A.P. to take a number as an input from the user and check if it is a Nest Number or not!
+[A nest number is a number which contains at least one digit which is zero]
+*/
+
+import java.util.Scanner;
+
+public class Q14
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		boolean nestNumber = false;
+		
+		System.out.print("Enter a number : ");
+		double num = sc.nextInt();
+		
+		while(num >= 10)
+		{
+			if(num % 10 == 0)
+			{
+				nestNumber = true;
+				break;
+			}
+			num = Math.floor(num / 10);
+		}
+		
+		if(nestNumber)
+		System.out.println("It is a nest number!");
+		
+		else
+		System.out.println("It is not a nest number!");
+	}
+}
