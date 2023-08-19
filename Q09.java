0c738b134971d77663d6c5620a9976861eb079d9
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-08-19 13:21:20 +0530

    Added solution of `Q9`

---
+++ Q09.java
@@ -0,0 +1,35 @@
+/*
+Question 9
+
+ ---------------------------------
+|  Days Late  | Late Fine(per day)|
+|             |                   |
+|   <= 5      |        10         |
+|> 5 and <= 10|        15         |
+|   > 10      |        30         |
+ ---------------------------------
+ 
+Take the number of days a member is late in returning a book as input and print the late fine.
+*/
+
+import java.util.Scanner;
+
+public class Q09
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Number of days late : ");
+		int daysLate = sc.nextInt();
+		
+		if(daysLate <= 5)
+		System.out.println("Late fine : " + (daysLate * 10));
+		
+		else if(daysLate > 5 && daysLate <= 10)
+		System.out.println("Late fine : " + (daysLate * 15));
+		
+		else
+		System.out.println("Late fine : " + (daysLate * 30));
+	}
+}
