c8dea9b33a8e1ac78b35eb1cd1ecc9a68490bfc7
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-08-19 11:23:42 +0530

    Added solution of `Q5`

---
+++ Q05.java
@@ -0,0 +1,20 @@
+/*
+Question 5
+
+W.A.P. to print all the ASCII values and their equivalent characters using a while loop.
+*/
+
+public class Q05
+{
+	public static void main(String args[])
+	{
+		System.out.println("The ASCII values and their equivalent characters are as follows :\n");
+		
+		int i = 0;
+		while(i <= 255)
+		{
+			System.out.println("ASCII Value: " + i + " | Character: " + (char)i + "\n");
+			i++;
+		}
+	}
+}
