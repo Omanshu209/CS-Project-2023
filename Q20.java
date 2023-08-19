6b36bd6ef015b6a7d2692d8d57fa3a99398d9a99
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-08-19 16:46:39 +0530

    Added solution of `Q20`

---
+++ Q20.java
@@ -0,0 +1,40 @@
+/*
+Question 20
+
+W.A.P. to take two numbers as input from the user. Check if they are negative. if not, then check whether they are perfect squares or not.
+*/
+
+import java.util.Scanner;
+
+public class Q20
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int[] numbers = new int[2];
+		
+		System.out.print("Number 1 : ");
+		numbers[0] = sc.nextInt();
+		
+		System.out.print("Number 2 : ");
+		numbers[1] = sc.nextInt();
+		
+		System.out.println();
+		
+		if(Math.sqrt(numbers[0]) % 1 == 0 && Math.sqrt(numbers[1]) % 1 == 0)
+		System.out.println("They are perfect square numbers!");
+		
+		else
+		for(int i = 0;i <= 1;i++)
+		{
+			if(numbers[i] < 0)
+			System.out.println(numbers[i] + " cannot be a perfect square as it is negative!");
+			
+			else if(Math.sqrt(numbers[i]) % 1 == 0)
+			System.out.println(numbers[i] + " is a perfect square number!");
+			
+			else
+			System.out.println(numbers[i] + " is not a perfect square number!");
+		}
+	}
+}
