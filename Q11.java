a7dee074166a2b57d6d0a31bf6099737682101ec
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-10-05 19:29:06 +0530

    Update `Q11.java`

--- Q11.java
+++ Q11.java
@@ -9,20 +9,20 @@ import java.util.Scanner;
 
 public class Q11
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Enter a number : ");
-		double num = sc.nextDouble(), firstDigit = num % 10;
+		double num = sc.nextDouble(), lastDigit = num % 10;
 		
 		while(num >= 10)
-		num = Math.floor(num / 10);
+			num = Math.floor(num / 10);
 		
-		if(firstDigit == num)
-		System.out.println("It is a cyclo number!");
+		if(lastDigit == num)
+			System.out.println("It is a cyclo number!");
 		
 		else
-		System.out.println("It is not a cyclo number!");
+			System.out.println("It is not a cyclo number!");
 	}
 }
