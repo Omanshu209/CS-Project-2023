34284dc990cd2befd5ccbf651698bc27620503fe
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-10-05 21:18:01 +0530

    Update `Q20.java`

--- Q20.java
+++ Q20.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Q20
 {
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		int[] numbers = new int[2];
@@ -24,13 +24,13 @@ public class Q20
 		for(int i = 0;i <= 1;i++)
 		{
 			if(numbers[i] < 0)
-			System.out.println(numbers[i] + " cannot be a perfect square as it is negative!");
+				System.out.println(numbers[i] + " cannot be a perfect square as it is negative!");
 			
 			else if(Math.sqrt(numbers[i]) % 1 == 0)
-			System.out.println(numbers[i] + " is a perfect square number!");
+				System.out.println(numbers[i] + " is a perfect square number!");
 			
 			else
-			System.out.println(numbers[i] + " is not a perfect square number!");
+				System.out.println(numbers[i] + " is not a perfect square number!");
 		}
 	}
 }
